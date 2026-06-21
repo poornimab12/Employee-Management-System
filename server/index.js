5f878440a1902416d99962f5b5d4164ca29df216
poornimab12 <poornimabalaji012@gmail.com>
2026-06-21 21:13:04 +0530

    HRMS project working

--- server/index.js
+++ server/index.js
@@ -1,88 +1,98 @@
 const express = require('express');
-const app = express();
 const mysql = require('mysql');
 const cors = require('cors');
+
+const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-
-//database connection
-const db = mysql.createConnection({ 
+// database connection (CREATE ONCE ONLY)
+const db = mysql.createConnection({
     user: 'root',
     host: 'localhost',
-    password: 'password',
+    password: 'root123',
     database: 'employeesys',
 });
 
-app.post('/create', (req, res) => {
- const name = req.body.name;
- const email = req.body.email;
- const age = req.body.age;
- const position = req.body.position;
- const ctc = req.body.ctc;
- //console.log(req.body);
-
-// connect to db and log the status
+// connect ONCE here
 db.connect((err) => {
     if (err) {
-        console.error('Error connecting to MySQL database:', err);
+        console.log("DB connection failed:");
+        console.log(err.message);
     } else {
         console.log('Connected to MySQL database');
     }
 });
 
- //db queries
- db.query(
-    'INSERT INTO employee (name, email, age, position, ctc) VALUES (?,?,?,?,?)',
-    [name, email, age, position, ctc], 
-    (err, result) => {                                  //what to do when fxn is executed
-        if(err){
-            console.log('Error executing SQL query:', err);
-            res.status(500).send('Internal Server Error');
-        } else {
-            res.send('values inserted');
+
+// CREATE
+app.post('/create', (req, res) => {
+    const { name, email, age, position, ctc } = req.body;
+
+    db.query(
+        'INSERT INTO employee (name, email, age, position, ctc) VALUES (?,?,?,?,?)',
+        [name, email, age, position, ctc],
+        (err, result) => {
+            if (err) {
+                console.log('SQL Error:', err);
+                return res.status(500).send('Internal Server Error');
+            }
+            return res.send('values inserted');
         }
-    });
+    );
 });
 
+
+// READ
 app.get('/employees', (req, res) => {
     db.query("SELECT * FROM employee", (err, result) => {
-        if(err){
-            console.log('Error executing SQL query (while getting employee list)', err);
-        }
-        else {
-            res.send(result);
-        }
-    })
-})
-
-app.put('/update', (req, res) => {
-    const id = req.body.id;
-    const ctc = req.body.ctc;
-    db.query("UPDATE employee SET ctc = ? WHERE id = ?", [ctc, id], (err, result) => {
-        if(err){
+        if (err) {
             console.log(err);
+            return res.status(500).send(err);
         }
-        else {
-            res.send(result);
-        }
+        res.send(result);
     });
 });
 
-app.delete('/delete/:id', (req,res) => {
-    const id = req.params.id;
-    db.query("DELETE FROM employee WHERE id =?", [id], (err, result) => {
-        if(err){
-            console.log(err);
-        }
-        else {
+
+// UPDATE
+app.put('/update', (req, res) => {
+    const { id, ctc } = req.body;
+
+    db.query(
+        "UPDATE employee SET ctc = ? WHERE id = ?",
+        [ctc, id],
+        (err, result) => {
+            if (err) {
+                console.log(err);
+                return res.status(500).send(err);
+            }
             res.send(result);
         }
-    });                                                                                         
+    );
 });
 
+
+// DELETE
+app.delete('/delete/:id', (req, res) => {
+    const id = req.params.id;
+
+    db.query(
+        "DELETE FROM employee WHERE id = ?",
+        [id],
+        (err, result) => {
+            if (err) {
+                console.log(err);
+                return res.status(500).send(err);
+            }
+            res.send(result);
+        }
+    );
+});
+
+
+// START SERVER
 app.listen(3001, () => {
     console.log('Server is running on port 3001');
 });
-
